af659636bf345fb8a45f3b78f74d23296aa014c4
seif7666 <seifeldin7666@gmail.com>
2020-02-19 18:17:52 +0200

    The assignment 1.

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main implements ICalculator {
+    public static final  int max=Integer.MAX_VALUE;
+    public static final  int min=Integer.MIN_VALUE;
+    @Override
+    public int add(int x, int y) {
+        long sum=(long)x+y;
+        if(sum<=max&&sum>=min)
+        return x + y;
+        System.out.println("The sum is out of bounds!");
+        return -1;
+    }
+
+    @Override
+    public float divide(int x, int y) {
+        try {
+            float z=x/y;
+            return (float) x /(float) y;
+        } catch (ArithmeticException e) {
+            throw new RuntimeException("Error .\nDivision by zero is undefined.");
+        }
+
+    }
+}
+interface ICalculator{
+    public int add(int x,int y);
+    public float divide(int x,int y)throws RuntimeException;
+}
+
